e4b83ceb3cc255ba1ec3f6d2ea824feda717fa74
David Kuruvilla <dapawn@gmail.com>
2017-10-18 14:06:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+require "pry"
+
+def badge_maker(name)
+  p "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each_with_object { |name, a| a << badge_maker(name) }
+end
+
+def assign_rooms(array)
+
+end
+
+def printer
+
+end
